9b31491edc5d32920b00807d701c968f4ed8466f
Electric_Sheep <iamthebou@gmail.com>
2019-01-30 15:01:34 +0100

    client now serving production version of the React app

---
+++ build/precache-manifest.9ee6f9835889b7f1d5a38d8bbf3fc869.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "36395aad69ef9b89a73df107dc6be10e",
+    "url": "/static/media/icone_upload.36395aad.png"
+  },
+  {
+    "revision": "f6f3d86b824fe613ed4a",
+    "url": "/static/css/main.92c4cb31.chunk.css"
+  },
+  {
+    "revision": "501a9be1e402ac1333cbe58c58e2811e",
+    "url": "/static/media/valider.501a9be1.svg"
+  },
+  {
+    "revision": "eaffe434e0410c26b0c1",
+    "url": "/static/js/1.eaffe434.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8ff3e7bbbec6a94ed0d6197a03451235",
+    "url": "/static/media/previous.8ff3e7bb.svg"
+  },
+  {
+    "revision": "318bf1dcfb19b68864722dcc401e85f4",
+    "url": "/static/media/icone_modifier.318bf1dc.png"
+  },
+  {
+    "revision": "6ef057a3a06fb49f1fee8ccc6dfdfdf9",
+    "url": "/static/media/icone_supprimer.6ef057a3.png"
+  },
+  {
+    "revision": "b766ca5d1a6e7bb7fa85128ab68dc604",
+    "url": "/static/media/tick.b766ca5d.png"
+  },
+  {
+    "revision": "f6f3d86b824fe613ed4a",
+    "url": "/static/js/main.f6f3d86b.chunk.js"
+  },
+  {
+    "revision": "285a5e8079ba5ec7f309b49359b6a6fc",
+    "url": "/static/media/icone_mon_compte.285a5e80.png"
+  },
+  {
+    "revision": "14908208e293e1c0d0bc84fad0450a5f",
+    "url": "/static/media/icone_creanciers.14908208.png"
+  },
+  {
+    "revision": "c2b26dc8607a8fecb3bb1d8cc88abe0a",
+    "url": "/static/media/icone_debiteurs.c2b26dc8.png"
+  },
+  {
+    "revision": "9b412cb9d9e26bd1976c52db1c6d2acb",
+    "url": "/static/media/icone_centre_actions.9b412cb9.png"
+  },
+  {
+    "revision": "6032ff8d592584d1be8097fef23ef7e5",
+    "url": "/static/media/icone_historique.6032ff8d.png"
+  },
+  {
+    "revision": "fb7a28d1a4b1aadb89a506c9f0cc9f90",
+    "url": "/static/media/contract.fb7a28d1.jpg"
+  },
+  {
+    "revision": "eaffe434e0410c26b0c1",
+    "url": "/static/css/1.5ff7c30a.chunk.css"
+  },
+  {
+    "revision": "797d18aab12a2ad47405dd96e1eeaef7",
+    "url": "/index.html"
+  }
+];
