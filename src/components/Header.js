d71660535a35ce47abe82eae41e0ad672cfe2ae8
Electric_Sheep <iamthebou@gmail.com>
2019-04-28 16:07:43 +0200

    Redeploying to new Heroku server

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,13 @@
 import React from "react";
 import "./Header.css";
 import { NavLink } from "react-router-dom";
+import Mason from "./images/mason.jpg";
 
 const Header = () => {
   return (
     <div>
-      {/* c'est un header */}
       <header className="header">
-        <img
-          src="http://www.arigoni-avocat.com/img/team-02.png"
-          className="avatar"
-          alt="avatar"
-        />
+        <img src={Mason} className="avatar" alt="avatar" />
         <NavLink to="/" className="deconnexion">
           Déconnexion
         </NavLink>
